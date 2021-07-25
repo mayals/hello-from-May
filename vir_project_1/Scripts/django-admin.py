c1503c52ab60da00420a7fbf2a0c51036e706ebc
mayals <developer.web.dj@gmail.com>
2021-07-25 06:39:38 +0300

    remove folder

--- vir_project_1/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\mayal\desktop\all_try\all_dj~1\projec~1\vir_pr~1\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
