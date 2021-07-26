5af53e71800d40076c85c383e4e693f03b61e072
mayals <developer.web.dj@gmail.com>
2021-07-26 06:36:21 +0300

    doing migrate for Task model

--- src/todo/models.py
+++ src/todo/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+# Utility that stores the date and time in our Timezone
+from django.utils import timezone
+# Imports the User - This maps the Task to the Users of our App.(Users as in Admin console)
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Task(models.Model):
+    T_name = models.CharField(max_length=60)
+
+    T_status = models.BooleanField(default=False)
+
+    T_published = models.DateTimeField(auto_now_add=True,auto_now=False)
+
+    T_user = models.ForeignKey(User, on_delete=models.CASCADE,related_name='U_tasks')
+
+    def __str__(self):
+        return self.T_name
+
+    class Meta:
+        ordering = ('-T_published',)
+
