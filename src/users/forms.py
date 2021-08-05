fa70a2d231e01ca8d306c9d27a5b5d3c3a8c8e59
mayals <developer.web.dj@gmail.com>
2021-08-06 02:08:08 +0300

    working with customize UserCreationForm

--- src/users/forms.py
+++ src/users/forms.py
@@ -1,4 +1,16 @@
-from django import forms
+from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django import forms
 
-class UsersForm(UserCreationForm):
+
+class UserCreationForm(forms.ModelForm):
+    username = forms.CharField(label='Username', max_length=30,help_text='space not allowed in username')
+    first_name = forms.CharField(label='First Name')
+    last_name = forms.CharField(label='Last Name')
+    email = forms.EmailField(label='Email')
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput(), min_length=8)
+    password2 = forms.CharField(label='Password Confirm', widget=forms.PasswordInput(), min_length=8)
+
+    class Meta:
+        model = User
+        fields = ('username','first_name','last_name','email','password1','password2',)
