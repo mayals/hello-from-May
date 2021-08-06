4f72c06ff547fbe2a1f2cb52bdc416a0da42ff29
mayals <developer.web.dj@gmail.com>
2021-08-06 16:30:54 +0300

    install Pillow and work with profile model

--- src/users/models.py
+++ src/users/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    P_image = models.ImageField(verbose_name='Profile Image',upload_to='prof_image',default='user_default',null=True,blank=True)
+    P_user = models.OneToOneField(User,on_delete=models.CASCADE,related_name = 'U_profile')
